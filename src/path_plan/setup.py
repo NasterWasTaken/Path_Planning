f2997b377de768e55a6f1ae898a20b3d5a44ee59
naster <nunocarvalhodonascimento@gmail.com>
2025-10-21 14:53:19 +0100

    Start

---
+++ src/path_plan/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+package_name = 'path_plan'
+
+entry_points={
+    'console_scripts': [
+        'plan_path = path_plan.path_plan.py:main',
+    ],
+},
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='naster',
+    maintainer_email='naster@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
